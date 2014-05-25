584fd9ac02ed5dbb51504ae1666568b031e38a6a
xatier <xatierlike@gmail.com>
2014-05-25 15:04:13 +0800

    change the order of some fields

--- proto.py
+++ proto.py
@@ -6,22 +6,27 @@ This file is the implementation of the TiTs protocol
 doc: https://ctlug.hackpad.com/TiTs-isnt-Telegram-sorry-TxiKrnafe2a
 
 Client to server header
+    message type: plaintext or encrypted
     length of the list   (special case: 1-to-1 = sender + receiver = 2)
     list of fingerprints
-    message type: plaintext or encrypted
 
 Client to client header (encrypted by sender's private key)
     timestamp
     serial number
     checksum
-    message type (MIME: text/plain or not)
     life time (how long does a message show on the client UI)
+        <= 0 second means display forever
     group ID (0 for 1-to-1 chatting)
+    message type (MIME: text/plain or not)
 
 payload (encrypted by receiver's public key)
     message body or attachment
 
 """
+import binascii
+import hashlib
+import struct
+import time
 
 def _valid_key_list(key_list):
     """ check all keys are valid """
@@ -39,15 +44,15 @@ class CSHeader:
     """ client to server header """
     def __init__(self, key_list, msg_type):
         """
+        msg_type: 0 for plain text, 1 for encrypted text
         n: the length of the key list, min = 2
         key_list: list of fingerprints, each fingerprints is a string of length 40
-        msg_type: 0 for plain text, 1 for encrypted text
         """
 
         if  _valid_key_list(key_list) and msg_type in [0, 1]:
+            self.msg_type = msg_type
             self.n = len(key_list)
             self.key_list = key_list
-            self.msg_type = msg_type
 
         else:
             raise ValueError("invalid key_list or msg_type")
@@ -55,15 +60,21 @@ class CSHeader:
     def encrypt(self):
         pass
 
+    def __repr__(self):
+        return "msg_type: {} n: {} \nkey_list: [{}]".format(
+               "encrypted" if self.msg_type else "plain text",
+               self.n,
+               ", ".join(self.key_list))
+
+
     def encode(self):
         """ encode the object to bytes
 
+        msg_type: unsigned char (1 byte)
         n: unsigned integer (4 bytes)
         key_list: 40*n char string
-        msg_type: unsigned char (1 byte)
         """
-        import struct
-        return struct.pack("I{}sB".format(self.n), self.n, "".join(self.key_list), self.msg_type)
+        return struct.pack("BI{}s".format(self.n*40), self.msg_type, self.n, "".join(self.key_list).encode())
 
     def __del(self):
         pass
@@ -72,20 +83,23 @@ class CSHeader:
 class CCHeader:
     """ client to client header """
 
-    def __init__(self, sn, mime, life_time, gid):
-        """ serial number, mine type, life time, group id """
-        import time
+    def __init__(self, sn, life_time, gid, mime):
+        """ serial number, life time, group id, mime """
         self.timestamp = time.time()
         self.sn = sn
-        # XXX: implement packet checksum with binascii.crc32()
-        # checksum for the payload or...?
         self.checksum = 0
-        self.mime = mime
         self.life_time = life_time
-        self.gid = gid
+        self.gid = hashlib.md5(gid.encode()).hexdigest().encode()
+        self.mime = mime
 
     def encrypt(self):
         pass
+
+    def __repr__(self):
+        return ("timestamp: {} serial number: {} checksum: {}\n" + \
+               "life_time: {} gid: {} mime: {}").format(
+               self.timestamp, self.sn, self.checksum,
+               self.life_time, self.gid, self.mime)
 
     def encode(self):
         """ encode the object to bytes
@@ -93,13 +107,13 @@ class CCHeader:
         timestamp: unsigned integer (4 bytes)
         serial number: unsigned integer (4 bytes)
         checksum: unsigned integer (4 bytes)
-        mime type: string
         lifetime: integer (4 bytes), -1 for no specific
         gid: 32 char string
+        mime type: string
         """
-        import struct
-        return struct.pack("III{}si32s".format(len(self.mime)),
-                self.timestamp, self.sn, self.checksum, self.mime, self.life_time, self.gid)
+        return struct.pack("fIIi32s{}s".format(len(self.mime)),
+                self.timestamp, self.sn, self.checksum,
+                self.life_time, self.gid, self.mime.encode())
 
 
     def __del(self):
@@ -119,6 +133,9 @@ class CCPayload:
     def encrypt(self):
         pass
 
+    def __repr__(self):
+        return payload
+
     def encode(self):
         return self.payload
 
@@ -133,12 +150,16 @@ class TiTsProto:
 
     def msg(self, n, key_list, msg_type, mine, life_time, gid, payload):
         """ return the byte object of the message """
-        self.sn += 1
-        cs_header = CSHeader(n, key_list, msg_type)
-        cc_header = CCHeader(self.sn, mime, life_time, gid)
-        cc_payload = CCPayload(payload)
 
-        # XXX: checksum here?
+        if not gid or type(gid) != type(''):
+            gid = "1to1"
+
+        self.sn += 1
+        cs_header = CSHeader(msg_type, key_list)
+        cc_header = CCHeader(self.sn, life_time, gid, mime)
+        cc_payload = CCPayload(payload)
+        cc_header.checksum = binascii.crc32(cc_payload.payload)
+
 
         # if use the session for both in group chating
         # otherwise:
@@ -167,30 +188,41 @@ class TiTsProto:
 
 # testing
 if __name__ == '__main__':
-    test_kl = ["7DD0628E6DB8D86466355BC094ABA8A15C7CC0BD",
-               "4ADD956CEF0703D685F2C1619A1C3CC9FBD057FC",
-               "D4E85D0DD0A59B4DFC91E71246526187F24D9E37",
-               "0B00B6BD1771B19E5B93902239A3B1BED944E4AB",
-               "B9A41E15A020D3CCF57A344BDA698A7F1B36B96F",
-               "69C9EC0C61A2F0C09DE296EB86F272230F1A804E",
-               "731B667E876258C9C946A7BF8ADE717C4726E54D",
-               "B66F3F18CEDA9D98EAC8844A63604D621DE9422B",
-               "607CB83DEA39F8382AF6CD95404FC0F0BC577A6E",
-               "0BDE82D3C6F2D4B41C0835A71D9893C4D8179092",
-               "602AD0A234EBFD13C7A81DFC48290D28562794F3",
-               "E7EC50252A41E6A9EDCB8A45A7413FD2504CF729",
-               "6CE565A4C2827E2C1B1FAC967ED8CD2F8049AC8B",
-               "5B2614662E4329E3433B3FEFE2A9EC26D765B010",
-               "A79A5FF7928432BD343E297CCB86B80E16F00F5D",
-               "D91FFE1E2266E5EC08182CCF2E040829F3F41888"]
+    # test_kl = ["7DD0628E6DB8D86466355BC094ABA8A15C7CC0BD",
+    #            "4ADD956CEF0703D685F2C1619A1C3CC9FBD057FC",
+    #            "D4E85D0DD0A59B4DFC91E71246526187F24D9E37",
+    #            "0B00B6BD1771B19E5B93902239A3B1BED944E4AB",
+    #            "B9A41E15A020D3CCF57A344BDA698A7F1B36B96F",
+    #            "69C9EC0C61A2F0C09DE296EB86F272230F1A804E",
+    #            "731B667E876258C9C946A7BF8ADE717C4726E54D",
+    #            "B66F3F18CEDA9D98EAC8844A63604D621DE9422B",
+    #            "607CB83DEA39F8382AF6CD95404FC0F0BC577A6E",
+    #            "0BDE82D3C6F2D4B41C0835A71D9893C4D8179092",
+    #            "602AD0A234EBFD13C7A81DFC48290D28562794F3",
+    #            "E7EC50252A41E6A9EDCB8A45A7413FD2504CF729",
+    #            "6CE565A4C2827E2C1B1FAC967ED8CD2F8049AC8B",
+    #            "5B2614662E4329E3433B3FEFE2A9EC26D765B010",
+    #            "A79A5FF7928432BD343E297CCB86B80E16F00F5D",
+    #            "D91FFE1E2266E5EC08182CCF2E040829F3F41888"]
 
-    for i in range(16):
-        # 1
-        # print(_valid_key_list(test_kl[:i]))
+    # for i in range(16):
+    #     # 1
+    #     # print(_valid_key_list(test_kl[:i]))
 
-        # 2
-        try:
-            cs_h = CSHeader(test_kl[:i], 2)
-        except ValueError as e:
-            print(i, e)
+    #     # 2
+    #     try:
+    #         cs_h = CSHeader(1, test_kl[:i])
+    #         print(cs_h)
+    #         print("->", end="")
+    #         print(cs_h.encode())
+    #     except ValueError as e:
+    #         print(i, e)
 
+    # 3
+    # for i in range(10):
+    #     mime = "text/blah{}".format(i)
+    #     cc_h = CCHeader(i, -1+i, "1to1", mime)
+    #     cc_h.checksum = binascii.crc32(b'blah')
+    #     print(cc_h)
+    #     print(cc_h.encode())
+    print("done")
