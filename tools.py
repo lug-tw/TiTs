575ae0b10cd84ab11a1f8f3cd08e0cfa91093c4d
xatier <xatierlike@gmail.com>
2014-05-28 00:03:12 +0800

    add sign and verify

--- tools.py
+++ tools.py
@@ -2,9 +2,12 @@ import gnupg
 import os
 import pprint
 
+MIT_KEY_SERVER = 'hkp://pgp.mit.edu'
+
 class _GPG:
     """
     a factory that wrappers python-gnupg
+    Note: most of input / output are byte data
     """
     def __init__(self, home=None):
         """
@@ -17,16 +20,29 @@ class _GPG:
 
     def encrypt(self, msg, fingerprint):
         """
-        encrypt `msg` with (receiver's) `fingerprint`, return encrypted **bytes data**
+        encrypt `msg` with (receiver's) `fingerprint`, return encrypted message
+
+        @msg: byte data
+        @return: byte data
         """
+
+        if type(msg) != type(b''):
+            raise ValueError("msg should be a byte object!")
+
+        if type(fingerprint) != type('') or len(fingerprint) != 40:
+            raise ValueError("invalid fingerprint!")
+
         return self.gpg.encrypt(msg, fingerprint).data
 
 
     def decrypt(self, msg):
         """
-        decrypt **byte data** `msg` with my private key
-        return an decrypted **bytes data**
+        decrypt **byte data** `msg` with my private key, return decrypted message
+
+        @msg: byte data
+        @return: byte data
         """
+
         if type(msg) != type(b''):
             raise ValueError("msg should be a byte object!")
 
@@ -34,15 +50,46 @@ class _GPG:
 
 
     def sign(self, msg):
-        pass
+        """
+        sign `msg` with my private key, return `msg+signature`
+
+        @msg: byte data
+        @return: byte data
+        """
+
+        if type(msg) != type(b''):
+            raise ValueError("msg should be a byte object!")
+
+        return self.gpg.sign(msg)
 
 
     def verify(self, sig, fingerprint):
-        pass
+        """
+        verify `sig` with `fingerprint`
+        Note: `fingerprint` should be available on MIT_KEY_SERVER
+
+        @sig: byte data
+        @return: byte data
+        """
+
+        if type(sig) != type(b''):
+            raise ValueError("msg should be a byte object!")
+
+        if type(fingerprint) != type('') or len(fingerprint) != 40:
+            raise ValueError("invalid fingerprint!")
+
+
+        try:
+            self.gpg.recv_keys(MIT_KEY_SERVER, fingerprint)
+
+        except:
+            raise ValueError("error when receiving fingerprint {}".format(fingerprint))
+
+        return self.gpg.verify(sig).valid
 
 
     def fingerprint(self):
-        """ return the first fingerprint of this gpg instancde """
+        """ return the **first** fingerprint of this gpg instancde """
         return self.gpg.list_keys()[0]['fingerprint']
 
 
@@ -54,3 +101,11 @@ if __name__ == '__main__':
     hidden = g.encrypt("test", g.fingerprint())
     pprint.pprint(hidden)
     pprint.pprint(g.decrypt(hidden))
+
+
+    """
+    debugging in IPython shell...
+    a = gnupg.GPG(gnupghome='test')
+    a.gen_key(a.gen_key_input(name_real='blah', name_email='gg@inin.de'))
+
+    """
