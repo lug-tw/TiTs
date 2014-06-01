31ccfd614c1a6179941ce5205cadd5947e108d12
xatier <xatierlike@gmail.com>
2014-06-01 16:49:49 +0800

    refactoring
    use session to protect inner packet
    
    use binary value instead of char array for key list

--- proto.py
+++ proto.py
@@ -40,10 +40,15 @@ def _valid_key_list(key_list):
 
     return True
 
+def _msg_encrypt(msg, key):
+    """ encrypt message with key """
+    # XXX: which algorithm should we use?
+    return msg
+
 
 class CSHeader:
     """ client to server header """
-    def __init__(self, key_list, msg_type):
+    def __init__(self, msg_type, key_list):
         """
         msg_type: 0 for plain text, 1 for encrypted text
         n: the length of the key list, min = 2
@@ -58,8 +63,6 @@ class CSHeader:
         else:
             raise ValueError("invalid key_list or msg_type")
 
-    def encrypt(self):
-        pass
 
     def __repr__(self):
         return "msg_type: {} n: {} \nkey_list: [{}]".format(
@@ -75,7 +78,9 @@ class CSHeader:
         n: unsigned integer (4 bytes)
         key_list: 40*n char string
         """
-        return struct.pack("BI{}s".format(self.n*40), self.msg_type, self.n, "".join(self.key_list).encode())
+        return struct.pack("BI{}s".format(self.n*20), self.msg_type, self.n,
+               b''.join([binascii.unhexlify(x) for x in self.key_list]))
+
 
     def __del(self):
         pass
@@ -96,14 +101,13 @@ class CCHeader:
                         ).encode()).hexdigest().encode()
         self.mime = mime
 
-    def encrypt(self):
-        pass
 
     def __repr__(self):
         return ("timestamp: {} serial number: {} checksum: {}\n" + \
                "life_time: {} gid: {} mime: {}").format(
                self.timestamp, self.sn, self.checksum,
                self.life_time, self.gid, self.mime)
+
 
     def encode(self):
         """ encode the object to bytes
@@ -134,8 +138,6 @@ class CCPayload:
         else:
             raise ValueError('payload should be str or bytes')
 
-    def encrypt(self):
-        pass
 
     def __repr__(self):
         return payload
@@ -151,12 +153,14 @@ class TiTsProto:
     """ the TiTs protocol """
     def __init__(self):
         self.sn = 0
+        self.session_key = b''
 
-    def msg(self, n, key_list, msg_type, mine, life_time, gid, payload):
-        """ return the byte object of the message """
+    def msg(self, n, key_list, msg_type, mime, life_time, gid, payload):
+        """ return the byte object (packet) of the message """
 
         if not gid or type(gid) != type(''):
             gid = "1to1"
+
 
         self.sn += 1
         cs_header = CSHeader(msg_type, key_list)
@@ -164,50 +168,44 @@ class TiTsProto:
         cc_payload = CCPayload(payload)
         cc_header.checksum = binascii.crc32(cc_payload.payload)
 
+        # all secure chat would be encrypted by the session key
 
-        # if use the session for both in group chating
-        # otherwise:
-        # cc_header: signed by sender
-        # cc_payload: encrypted by receiver's public key
+        outer = cs_header.encode()
+        inner = cc_header.encode() + cc_payload.encode()
 
-        # XXX: fill this part with gpg keys
-        sender_pri = ""
-        recv_pub = ""
-        session_key = ""
+        if cs_header.msg_type:
+            inner = _msg_encrypt(inner, self.session_key)
 
-        if cc_header.msg_type:
-            if cs_header.n == 2:
-                cc_header.encrypt(sender_pri)
-                cc_payload.encrypt(recv_pub)
+        packet = outer + inner
+        print("len = {}".format(len(packet)))
 
-            else:
-                cc_header.encrypt(session_key)
-                cc_payload.encrypt(session_key)
+        return  struct.pack("I", len(packet)) + packet
 
-        return cs_header.encode() + cc_header.encode() + cc_payload().encode()
 
+    def decode(self):
+        pass
 
     def __del__(self):
         pass
 
 # testing
 if __name__ == '__main__':
-    # test_kl = ["7DD0628E6DB8D86466355BC094ABA8A15C7CC0BD",
-    #            "4ADD956CEF0703D685F2C1619A1C3CC9FBD057FC",
-    #            "D4E85D0DD0A59B4DFC91E71246526187F24D9E37",
-    #            "0B00B6BD1771B19E5B93902239A3B1BED944E4AB",
-    #            "B9A41E15A020D3CCF57A344BDA698A7F1B36B96F",
-    #            "69C9EC0C61A2F0C09DE296EB86F272230F1A804E",
-    #            "731B667E876258C9C946A7BF8ADE717C4726E54D",
-    #            "B66F3F18CEDA9D98EAC8844A63604D621DE9422B",
-    #            "607CB83DEA39F8382AF6CD95404FC0F0BC577A6E",
-    #            "0BDE82D3C6F2D4B41C0835A71D9893C4D8179092",
-    #            "602AD0A234EBFD13C7A81DFC48290D28562794F3",
-    #            "E7EC50252A41E6A9EDCB8A45A7413FD2504CF729",
-    #            "6CE565A4C2827E2C1B1FAC967ED8CD2F8049AC8B",
-    #            "5B2614662E4329E3433B3FEFE2A9EC26D765B010",
-    #            "A79A5FF7928432BD343E297CCB86B80E16F00F5D",
-    #            "D91FFE1E2266E5EC08182CCF2E040829F3F41888"]
+    test_kl = ["7DD0628E6DB8D86466355BC094ABA8A15C7CC0BD",
+               "4ADD956CEF0703D685F2C1619A1C3CC9FBD057FC",
+               "D4E85D0DD0A59B4DFC91E71246526187F24D9E37",
+               "0B00B6BD1771B19E5B93902239A3B1BED944E4AB",
+               "B9A41E15A020D3CCF57A344BDA698A7F1B36B96F",
+               "69C9EC0C61A2F0C09DE296EB86F272230F1A804E",
+               "731B667E876258C9C946A7BF8ADE717C4726E54D",
+               "B66F3F18CEDA9D98EAC8844A63604D621DE9422B",
+               "607CB83DEA39F8382AF6CD95404FC0F0BC577A6E",
+               "0BDE82D3C6F2D4B41C0835A71D9893C4D8179092",
+               "602AD0A234EBFD13C7A81DFC48290D28562794F3",
+               "E7EC50252A41E6A9EDCB8A45A7413FD2504CF729",
+               "6CE565A4C2827E2C1B1FAC967ED8CD2F8049AC8B",
+               "5B2614662E4329E3433B3FEFE2A9EC26D765B010",
+               "A79A5FF7928432BD343E297CCB86B80E16F00F5D",
+               "D91FFE1E2266E5EC08182CCF2E040829F3F41888"]
 
     # for i in range(16):
     #     # 1
@@ -229,4 +227,8 @@ if __name__ == '__main__':
     #     cc_h.checksum = binascii.crc32(b'blah')
     #     print(cc_h)
     #     print(cc_h.encode())
+
+    # 4
+    # tit = TiTsProto()
+    # print(tit.msg(2, test_kl[:], 0, "text/plain", 1, None, b'5566'))
     print("done")
